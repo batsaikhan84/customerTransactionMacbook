6059efb274251da8a9a2e0ecc45869e6e3ba383f
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2020-12-11 10:20:40 -0600

    transactions delete mapping organized

--- src/main/java/com/customer/relationship/controller/TransactionController.java
+++ src/main/java/com/customer/relationship/controller/TransactionController.java
@@ -2,13 +2,17 @@ package com.customer.relationship.controller;
 
 import com.customer.relationship.Repository.TransactionRepository;
 import com.customer.relationship.Repository.UserRepository;
+import com.customer.relationship.exception.CustomerTransactionNotFoundException;
+import com.customer.relationship.exception.UserNotFoundException;
 import com.customer.relationship.model.Transactions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TransactionController {
@@ -16,44 +20,45 @@ public class TransactionController {
     private TransactionRepository transactionRepository;
     @Autowired
     private UserRepository userRepository;
-//    @GetMapping("/transactions")
-//    public Page<Transactions> getTransactions(Pageable pageable) {
-//        return transactionRepository.findAll(pageable);
-//    }
 
     @GetMapping("/users/{userId}/transactions")
     public Page<Transactions> getTransactionsByUser(@PathVariable (value = "userId") Long userId, Pageable pageable) {
         return transactionRepository.findByUserId(userId, pageable);
     }
-//    @PostMapping("/transactions")
-//    public Transactions addTransaction(@RequestBody Transactions transactions) {
-//        return transactionRepository.save(transactions);
-//    }
+    @GetMapping("/users/{userId}/transactions/{transactionId}")
+    public ResponseEntity<Transactions> getTransaction(@PathVariable(value = "userId") Long userId, @PathVariable(value = "transactionId") Long transactionId) {
+        if(!userRepository.existsById(userId)) {
+            throw new UserNotFoundException("ID: " + userId + " not found");
+        }
+        return transactionRepository.findByIdAndUserId(transactionId, userId).map(transaction -> ResponseEntity.ok().body(transaction)).
+                orElseThrow(() -> new CustomerTransactionNotFoundException("ID: " + transactionId + " not found"));
+    }
     @PostMapping("/users/{userId}/transactions")
-    public Optional<Transactions> addTransaction(@PathVariable (value = "userId") Long userId, @RequestBody Transactions transactions) {
+    public Transactions addTransaction(@PathVariable (value = "userId") Long userId, @RequestBody Transactions transactions) {
         return userRepository.findById(userId).map(user -> {
             transactions.setUser(user);
             return transactionRepository.save(transactions);
-        });
+        }).orElseThrow(() -> new CustomerTransactionNotFoundException("Transaction not found"));
     }
-//    @GetMapping("/transactions/{id}")
-//    public ResponseEntity<Transactions> getUser(@PathVariable(value = "id") long id) throws TransactionNotFoundException {
-//        Transactions transactions = transactionRepository.findById(id).orElseThrow(() -> new TransactionNotFoundException("ID: " + id + " not found"));
-//        return ResponseEntity.ok().body(transactions);
-//    }
-//    @DeleteMapping("/transactions/{id}")
-//    public Map<String, String> deleteUser(@PathVariable(value = "id") long id) throws RecordNotFoundException {
-//        Transactions transactions = transactionRepository.findById(id).orElseThrow(() -> new RecordNotFoundException("ID: " + id + " not found"));
-//        transactionRepository.delete(transactions);
-//        Map<String, String> deleteUser = new HashMap<>();
-//        deleteUser.put("ID: ", id + " deleted");
-//        return deleteUser;
-//    }
-//    @PutMapping("/transactions/{id}")
-//    public ResponseEntity<Transactions> updateTransaction(@PathVariable(value = "id") long id, @RequestBody Transactions transactionDetails) throws RecordNotFoundException {
-//        Transactions transactions = transactionRepository.findById(id).orElseThrow(() -> new RecordNotFoundException("ID: " + id + " not found"));
-//        transactions.setAmount(transactionDetails.getAmount());
-//        final Transactions updateTransaction = transactionRepository.save(transactions);
-//        return ResponseEntity.ok(updateTransaction);
-//    }
+    @PutMapping("/users/{userId}/transactions/{transactionId}")
+    public Transactions updateTransaction(@PathVariable(value = "userId") Long userId, @PathVariable(value = "transactionId") Long transactionId,
+                                          @RequestBody Transactions transactionDetails) {
+        if (!userRepository.existsById(userId)) {
+            throw new UserNotFoundException("ID: " + userId + " not found");
+        }
+        return transactionRepository.findById(transactionId).map(transaction -> {
+            transaction.setAmount(transactionDetails.getAmount());
+            return transactionRepository.save(transaction);
+        }).orElseThrow(() -> new CustomerTransactionNotFoundException("ID: " + userId + " not found"));
+    }
+    @DeleteMapping("/users/{userId}/transactions/{transactionId}")
+    public Map<String, String> deleteTransaction(@PathVariable(value = "userId") Long userId, @PathVariable(value = "transactionId") Long transactionId,
+                                                 @RequestBody Transactions transactionDetails) {
+        return transactionRepository.findByIdAndUserId(transactionId, userId).map(transaction -> {
+            transactionRepository.delete(transaction);
+            Map<String, String> deleteTransaction = new HashMap<>();
+            deleteTransaction.put("ID: " + userId, " deleted");
+            return deleteTransaction;
+        }).orElseThrow(() -> new CustomerTransactionNotFoundException("ID: " + transactionId + " not found"));
+    }
 }
