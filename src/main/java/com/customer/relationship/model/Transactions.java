7d65f527f6d0d7aa778ebf1d3bc4bbc3a6a882eb
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2020-12-11 10:20:33 -0600

    transactions delete mapping organized

--- src/main/java/com/customer/relationship/model/Transactions.java
+++ src/main/java/com/customer/relationship/model/Transactions.java
@@ -1,9 +1,12 @@
 package com.customer.relationship.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "transaction_table")
+@JsonIgnoreProperties(value = {"users"})
 public class Transactions extends AuditModel{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
